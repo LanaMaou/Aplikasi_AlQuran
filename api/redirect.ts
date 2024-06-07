176e5e69161785de0d91dcbd76b4659fc659da9b
LanaMaou <maulanarasyit777@gmail.com>
2024-06-07 22:51:43 +0800

    test lagi

--- api/redirect.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export default async (req: any, res: any) => {
-  // Redirect all requests to index.html
-  res.writeHead(308, { Location: "/" });
-  res.end();
-};
